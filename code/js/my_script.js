14a26d8bb922261332430f1b7562ccfd77d6abaf
Timothy Nwokeji <nwokejitimoth00@gmail.com>
2022-12-07 11:36:31 +0100

    Changes to resume file name and works

--- code/js/my_script.js
+++ code/js/my_script.js
@@ -1,49 +1,59 @@
 const styles=document.querySelector(".styles");
 const contain=document.querySelector(".contain");
 const projects=[
-
-   {
-      image:"E-commerce_Page",
-      text:"A Functional E-commerce Webpage",
-      link:"https://darq-envoy.github.io/e_commerce_webpage/",
-     repo: "https://github.com/DARQ-Envoy/e_commerce_webpage",
-  },
-  {
+{
+   image:"calculator_image",
+   text:"A calculator built with javaScript and python",
+   link:"https://timothynwokeji.pythonanywhere.com/",
+   repo: "https://github.com/DARQ-Envoy/Calculator-js",
+},
+{
+   image:"countdown_timer",
+   text:"A countdown timer built with React",
+   link:"https://darq-envoy.github.io/Countdown-timer/",
+  repo: "https://github.com/DARQ-Envoy/Countdown-timer",
+},
+{
+   image:"9jaGames",
+   text:"A fully functional webpage",
+   link:"https://darq-envoy.github.io/9jia-games-landing-page/",
+  repo: "https://github.com/DARQ-Envoy/9jia-games-landing-page",
+},
+{
+   image:"MetaFetch_Convert",
+   text:"The Convert File Page for MetaFetch",
+   link:"https://darq-envoy.github.io/Metafetch_Convert_Page/",
+  repo: "https://github.com/DARQ-Envoy/Metafetch_Convert_Page",
+},
+{
    image:"desktop-screenshot",
    text:"Easybank Landing Page",
    link:"https://darq-envoy.github.io/Easy-bank/",
   repo: "https://github.com/DARQ-Envoy/Easy-bank",
 }, 
 {
-    image:"MetaFetch_Landing_page",
-    text:"The Landing Page for MetaFetch",
-    link:"https://darq-envoy.github.io/MetaFetch-Landing-Page/",
-   repo: "https://github.com/DARQ-Envoy/MetaFetch-Landing-Page",
+   image:"Creatives_page",
+   text:"Sunnyside Landing Page",
+   link:"https://darq-envoy.github.io/Sunny_side_Landing_page/",
+  repo: "https://github.com/DARQ-Envoy/Sunny_side_Landing_page",
 },
+{
+      image:"E-commerce_Page",
+      text:"A Functional E-commerce Webpage",
+      link:"https://darq-envoy.github.io/e_commerce_webpage/",
+     repo: "https://github.com/DARQ-Envoy/e_commerce_webpage",
+  },
 {
    image:"huddle_landing_page",
    text:"Huddle Landing Page",
    link:"https://darq-envoy.github.io/Huddle-Home-page/",
   repo: "https://github.com/DARQ-Envoy/Huddle-Home-page",
 },
-
- {
-    image:"MetaFetch_Convert",
-    text:"The Convert File Page for MetaFetch",
-    link:"https://darq-envoy.github.io/Metafetch_Convert_Page/",
-   repo: "https://github.com/DARQ-Envoy/Metafetch_Convert_Page",
-},
 {
     image:"Social_Page",
     text:"Social Media Dashboard",
     link:"https://darq-envoy.github.io/Social_Media_Dashboard/",
    repo: "https://github.com/DARQ-Envoy/Social_Media_Dashboard",
-},
-{
-   image:"Creatives_page",
-   text:"Sunnyside Landing Page",
-   link:"https://darq-envoy.github.io/Sunny_side_Landing_page/",
-  repo: "https://github.com/DARQ-Envoy/Sunny_side_Landing_page",
 }
 
 ]
